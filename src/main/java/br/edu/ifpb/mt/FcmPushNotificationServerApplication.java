da1d1c00b256caaef66f9bd8b91c2743b6faa4e2
Franck Aragão <fjunior.aragao@gmail.com>
2017-02-14 20:08:28 -0300

    first commit

---
+++ src/main/java/br/edu/ifpb/mt/FcmPushNotificationServerApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifpb.mt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmPushNotificationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcmPushNotificationServerApplication.class, args);
+	}
+}
